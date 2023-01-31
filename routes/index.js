8f47317f1f10f66f76aea8a832e5cdcde8ca3fed
Rallve <lucas.liedgren@gmail.com>
2023-01-31 14:59:07 +0100

    gjorde saker

--- routes/index.js
+++ routes/index.js
@@ -36,8 +36,26 @@ router.get('/new', async function (req, res, next) {
 
 router.post('/new', async function (req, res, next) {
     const { author, title, content } = req.body;
-    const [rows] = await promisePool.query("INSERT INTO DITT_TABELL_NAMN (author, title, content) VALUES (?, ?, ?)", [author, title, content]);
+    const [rows] = await promisePool.query('INSERT INTO lgl23forum (author, title, content) VALUES (?, ?, ?)', [1, title, content]);
     res.redirect('/');
+});
+
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+
+    // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
+    let user = await promisePool.query('SELECT * FROM din_tabell WHERE name = ?', [author]);
+    if (!user) {
+        user = await promisePool.query('INSERT INTO din_tabell (name) VALUES (?)', [author]);
+    }
+
+    // user.insertId bör innehålla det nya ID:t för författaren
+
+    const userId = user.insertId || user[0].id;
+
+    // kör frågan för att skapa ett nytt inlägg
+    const [rows] = await promisePool.query('INSERT INTO din_tabell (author, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
 });
 
 module.exports = router;
